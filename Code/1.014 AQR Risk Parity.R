720fb7c192ad23adc89e863b9ed7d6d2a3032b43
luyongxu <luyongxu@gmail.com>
2016-10-13 02:43:48 -0400

    Add 1.014

---
+++ Code/1.014 AQR Risk Parity.R
@@ -0,0 +1,42 @@
+source("./Code/1.001 Initial Functions and Libraries.R")
+
+# 1. Load data. 
+data_raw <- getSymbolsYahooMany(c("AQRNX", "TLO", "SCHP", "SPY", "GLD", "EFA", "VWO", "EMB", "GSG", 
+                                  "LQD", "HYG"))
+
+# 2. Calculate daily returns
+data <- data_raw %>% 
+  group_by(ticker) %>% 
+  mutate(daily_return = adjusted_close / lag(adjusted_close, 1) - 1) %>% 
+  filter(date >= "2010-10-01") %>% 
+  dcast(date ~ ticker, value.var = "daily_return")
+
+# 3. Train model. 
+m01 <- lm(AQRNX ~ 0 + GLD + SCHP + SPY + TLO + EFA + VWO + EMB + GSG, data = data)
+summary(m01)
+sum(coef(m01))
+
+# 4. Plot returns. 
+returns <- data %>% 
+  mutate(AQRNX = ifelse(is.na(AQRNX), 0, AQRNX), 
+         AQR = cumprod(1 + AQRNX) - 1, 
+         m01 = cumprod(1 + predict(m01, data)) - 1) %>% 
+  melt(id.vars = "date") %>% 
+  filter(variable %in% c("AQR", "m01"))
+(p1 <- ggplot(returns, aes(x = date, y = value, colour = variable)) + 
+  geom_line() + 
+  labs(title = "Reverse Engineering AQR's Risk Parity Strategy", 
+       subtitle = "There is a 96% correlation between AQR's risk parity strategy and the synthetic strategy.", 
+       y = "Return", 
+       x = "Date") + 
+  theme_alphaplot() + 
+  scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 year", date_labels = "%Y") + 
+  scale_colour_discrete("Strategy", 
+                        breaks = c("AQR", "m01"), 
+                        labels = c("AQR", "Synthetic")))
+ggsave(file = "./Plots/1.014 AQR Risk Parity.png", plot = p1, dpi = 300, width = 8, height = 5)
+
+# 5. AQR Portolio Holdings. 
+holdings <- read_csv("./Raw Data/Risk Parity/AQR Risk Parity Fund Holdings.csv") %>% 
+  select(c(1, 2, 5))
+print(xtable(holdings), type = "html")
